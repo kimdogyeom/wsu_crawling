8591d58ea2837cccc583811af44a80b75f291896
dogyeom <rlaehrua930@naver.com>
2024-11-12 17:43:54 +0900

    first init

---
+++ src/main/java/dogyeom/wsu_crawling/WsuCrawlingApplication.java
@@ -0,0 +1,13 @@
+package dogyeom.wsu_crawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsuCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsuCrawlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dogyeom/wsu_crawling/WsuCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package dogyeom.wsu_crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsuCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

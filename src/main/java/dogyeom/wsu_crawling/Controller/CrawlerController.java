4aab42f1033ed8f3445b5ee2b3b27b08075da034
dogyeom <rlaehrua930@naver.com>
2024-11-17 21:29:44 +0900

    fix: CrawlerController 어노테이션 수정 및 크롤링 api 변경
    /api/crawl/start의 target url을 받는 부분을 PathVariable에서 RequestParam으로 변경: target url을 그대로 받기 때문에 example?ex=10"같은 url에서 서식이 깨지는 오류가 발생

--- src/main/java/dogyeom/wsu_crawling/Controller/CrawlerController.java
+++ src/main/java/dogyeom/wsu_crawling/Controller/CrawlerController.java
@@ -5,22 +5,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/api/crawl")
+@RestController
+@RequestMapping("/api/crawl")
 public class CrawlerController {
 
 	private final CrawlerService crawlerService;
 
-
-	@PostMapping("/start/{target}")
-	public ResponseEntity<?> ruleCrawler(@PathVariable("target") String target) {
+	@PostMapping("/start")
+	public ResponseEntity<String> ruleCrawler(@RequestParam String target) {
 		crawlerService.startCrawling(target);
 
 		return ResponseEntity.ok("크롤링 시작");
 	}
-
 
 
 }
